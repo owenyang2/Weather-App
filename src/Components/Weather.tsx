3201b2fea4a9c2bc6e7e485f103422dfe0373ee7
Owen <82738322+owenyang2@users.noreply.github.com>
2025-05-04 13:53:55 -0400

    Change location selection to dropdown

--- src/Components/Weather.tsx
+++ src/Components/Weather.tsx
@@ -3,12 +3,12 @@ import { useState, useEffect } from 'react';
 
 import './Weather.css'
 
-let LOCATIONS: {[location: string]: {[key: string]: number;}} = {
+let LOCATIONS: { [location: string]: { [key: string]: number; } } = {
 	Toronto: {
 		"latitude": 43.6532,
-		"longitude": -79.3832	
+		"longitude": -79.3832
 	},
-	
+
 	Waterloo: {
 		"latitude": 43.4668,
 		"longitude": -80.5164
@@ -32,15 +32,15 @@ async function updateWeather(location: string) {
 		"timezone": "America/New_York",
 		"forecast_days": 1
 	};
-	
+
 	const url = "https://api.open-meteo.com/v1/forecast";
 	const response = (await fetchWeatherApi(url, params))[0];
 	const daily = response.daily()!;
-	
+
 	return {
 		tempMax: daily.variables(0)!.valuesArray()![0],
 		tempMin: daily.variables(1)!.valuesArray()![0],
-	};	
+	};
 }
 
 function Weather() {
@@ -48,31 +48,36 @@ function Weather() {
 	const [weatherData, setWeatherData] = useState<{ tempMax: number; tempMin: number } | null>(null);
 
 	useEffect(() => {
-	  updateWeather(currLocation).then((data) => {
-		if (data) setWeatherData(data);
-	  });
+		updateWeather(currLocation).then((data) => {
+			if (data) setWeatherData(data);
+		});
 	}, [currLocation]); // rerun when currLocation changes  
 
 	return (
-	  <div className="centered">
-		{weatherData ? (
-		  <>
-		  <div className="weather-display">
-			<h1 className="weather-title">{currLocation}</h1>
-			<p>High: {Math.round(weatherData.tempMax)}°</p>
-			<p>Low: {Math.round(weatherData.tempMin)}°</p>
-		  </div>
-		  <div className="location-switch">
-		  	{Object.keys(LOCATIONS).map((location: string) =>
-				<button className="btn btn-primary" key={location} onClick={() => {setCurrLocation(location)}}>{location}</button>
+		<div className="centered">
+			{weatherData ? (
+				<>
+					<div className="weather-display">
+						<h1 className="weather-title">{currLocation}</h1>
+						<p>High: {Math.round(weatherData.tempMax)}°</p>
+						<p>Low: {Math.round(weatherData.tempMin)}°</p>
+					</div>
+					<div className="dropdown">
+						<button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Change Location
+						</button>
+						<ul className="dropdown-menu">
+							{Object.keys(LOCATIONS).map((location: string) =>
+								<li><button className="dropdown-item" key={location} onClick={() => { setCurrLocation(location) }}>{location}</button></li>
+							)}
+						</ul>
+					</div>
+				</>
+			) : (
+				<p>Loading...</p>
 			)}
-		  </div>
-		  </>
-		) : (
-		  <p>Loading...</p>
-		)}
-	  </div>
+		</div>
 	);
 }
-  
+
 export default Weather;
