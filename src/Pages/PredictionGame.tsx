2f114d639e7f126ef2b8ae3c156a3b6deade28ec
Owen <82738322+owenyang2@users.noreply.github.com>
2025-05-04 19:55:35 -0400

    Add prediction updating functionality

--- src/Pages/PredictionGame.tsx
+++ src/Pages/PredictionGame.tsx
@@ -1,19 +1,34 @@
+import { useState } from 'react';
 import './PredictionGame.css'
 
 function PredictionGame() {
+    const [currentPrediction, setPrediction] = useState<number | null>(null);
+
+    function submitPrediction(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const form = event.target as HTMLFormElement;
+        const formData = new FormData(form);    
+        const formJson = Object.fromEntries(formData.entries());
+        
+        setPrediction(Number(formJson.prediction)); // update prediction
+    }
+
     return (
         <div className="main-content">
             <ul className="list-group">
                 <li className="list-group-item">
                     <h2>Today's Prediction</h2>
-                    <form>
+                    <form onSubmit={submitPrediction}>
                         <div className="prediction-form">
-                            <label htmlFor="pred-select" className="form-label">Enter Your Prediction for Tomorrow's High Temperature in Toronto</label>
+                            <label htmlFor="temp-input" className="form-label">Enter Your Prediction for Tomorrow's High Temperature in Toronto</label>
                             <div className="input-with-unit">
-                                <input type="number" className="form-control pred-select" id="pred-select" aria-describedby="current-pred" />
+                                <input name="prediction" type="number" className="form-control" id="temp-input" aria-describedby="current-pred" />
                                 <span className="unit">°C</span>
+                                <button type="submit" className="btn btn-primary">Submit</button>
                             </div>
-                            <div id="current-pred" className="form-text">Current Prediction: N/A</div>
+                            <div id="current-pred" className="form-text">Current Prediction: {currentPrediction ? currentPrediction + "°C" : "N/A"}
+                            </div>
                         </div>
                     </form>
                 </li>
